2c3ebe1537b7208ac7d6eb90419cf1368f6089b7
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-11-22 19:22:28 +0300

    123

--- src/main/java/com/example/spring160/controllers/AllItemsController.java
+++ src/main/java/com/example/spring160/controllers/AllItemsController.java
@@ -1,6 +1,7 @@
 package com.example.spring160.controllers;
 
 import com.example.spring160.models.ItemModel;
+import com.example.spring160.models.enums.Type;
 import com.example.spring160.repos.ItemRepo;
 import com.example.spring160.services.FirebaseImageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/items")
@@ -31,6 +33,59 @@ public class AllItemsController {
         for (ItemModel i: list){
             i.setUrl(firebaseImageService.getPhotoUrl(i.getUrl()));
         }
+        model.addAttribute("list", list);
+        return "allItems";
+    }
+    @GetMapping("/tress")
+    public String getTress(Model model){
+        List<ItemModel> list = itemRepo.findAll();
+        for (ItemModel i: list){
+            i.setUrl(firebaseImageService.getPhotoUrl(i.getUrl()));
+        }
+        list = list.stream().filter(i -> i.getType().equals(Type.CHRISTMAS_TREES)).collect(Collectors.toList());
+        model.addAttribute("list", list);
+        return "allItems";
+    }
+
+    @GetMapping("/toys")
+    public String getToys(Model model){
+        List<ItemModel> list = itemRepo.findAll();
+        for (ItemModel i: list){
+            i.setUrl(firebaseImageService.getPhotoUrl(i.getUrl()));
+        }
+        list = list.stream().filter(i -> i.getType().equals(Type.TOYS)).collect(Collectors.toList());
+        model.addAttribute("list", list);
+        return "allItems";
+    }
+
+    @GetMapping("/decorations")
+    public String getDec(Model model){
+        List<ItemModel> list = itemRepo.findAll();
+        for (ItemModel i: list){
+            i.setUrl(firebaseImageService.getPhotoUrl(i.getUrl()));
+        }
+        list = list.stream().filter(i -> i.getType().equals(Type.DECORATIONS)).collect(Collectors.toList());
+        model.addAttribute("list", list);
+        return "allItems";
+    }
+
+    @GetMapping("/lights")
+    public String getLights(Model model){
+        List<ItemModel> list = itemRepo.findAll();
+        for (ItemModel i: list){
+            i.setUrl(firebaseImageService.getPhotoUrl(i.getUrl()));
+        }
+        list = list.stream().filter(i -> i.getType().equals(Type.LIGHTS)).collect(Collectors.toList());
+        model.addAttribute("list", list);
+        return "allItems";
+    }
+    @GetMapping("/presents")
+    public String getPresents(Model model){
+        List<ItemModel> list = itemRepo.findAll();
+        for (ItemModel i: list){
+            i.setUrl(firebaseImageService.getPhotoUrl(i.getUrl()));
+        }
+        list = list.stream().filter(i -> i.getType().equals(Type.PRESENTS)).collect(Collectors.toList());
         model.addAttribute("list", list);
         return "allItems";
     }
